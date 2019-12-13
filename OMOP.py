53c122e0b93d57311212176554c574d0b01edfcd
Alexander Jenke <alexander.jenke@tu-dresden.de>
2019-12-13 18:59:08 +0100

    add information about DB to OMOP

--- OMOP.py
+++ OMOP.py
@@ -2,6 +2,7 @@ import psycopg2 as db
 
 
 class DB:
+
     def __init__(self, dbname='OHDSI', user='ohdsi_admin_user', host='localhost', password='omop'):
         self.conn = db.connect(f"dbname='{dbname}' user='{user}' host='{host}' password='{password}'")
         self.cursor = self.conn.cursor()
@@ -13,6 +14,17 @@ class DB:
     def insert(self, sql: str):
         self.cursor.execute(sql)
         self.conn.commit()
+
+    def availabe_tables(self):
+        return [t[0] for t in
+                self.select("""select tablename From pg_catalog.pg_tables Where schemaname = 'p21_cdm'""")]
+
+    def availabel_colums(self, table):
+        return self.select(
+            """select column_name,data_type  from information_schema.columns  where table_name = '""" + table + """'""")
+
+    def __getattr__(self, item):
+        return "p21_cdm." + str(item)
 
 
 if __name__ == '__main__':
@@ -28,4 +40,3 @@ if __name__ == '__main__':
 
     res = db.select("""SELECT  * FROM p21_cdm.person """)
     print(res)
-
