bd95a95115fdfbe8bc4be41f7b1113f96d0296e5
Jenke, Alexander <alexander.jenke@uniklinikum-dresden.de>
2020-01-02 16:38:41 +0100

    Edit main

--- etl/main.py
+++ etl/main.py
@@ -1,15 +1,15 @@
 import pandas as pd
 from sys import argv
 import os
+from Classes import Person
 
-GENDER_D = {"m" : "8507", "w" : "8532", "nan" : "8551"}
-
+GENDER_D = {"m": "8507", "w": "8532", "nan": "8551"}
 
 if __name__ == "__main__":
     csv_dir = argv[1]
-    
+
     fall_csv = os.path.join(csv_dir, "FALL.csv")
-    icd_csv = os.path.join(csv_dir,  "ICD.csv")
+    icd_csv = os.path.join(csv_dir, "ICD.csv")
     messungen_csv = os.path.join(csv_dir, "MESSUNGEN.csv")
     labor_csv = os.path.join(csv_dir, "LABOR.csv")
     ops_csv = os.path.join(csv_dir, "OPS.csv")
@@ -20,20 +20,19 @@ if __name__ == "__main__":
     labor_csv = pd.read_csv(labor_csv, delimiter=";")
     ops_csv = pd.read_csv(ops_csv, delimiter=";")
 
-    person_d = {}
-    patient_ids = fall_pd["patienten_nummer"].unique()
-    print(patient_ids)
-    for id in patient_ids:
+    for id in fall_pd["patienten_nummer"].unique():
         df = fall_pd[fall_pd.patienten_nummer.isin([id])]
         df = df.sort_values(by=["aufnahmedatum"])
-        print(GENDER_D[str(df.iloc[0]["geschlecht"])])
-        location = {}
-        location["city"] = df.wohnort 
-        location["zip"] = df.plz
         last_record = df.iloc[-1]
-        person = Person(person_id=id, 
-                gender_concept_id=GENDER_ID[last_record["geschlecht"]], 
-                year_of_birth=last_record["geburtsjahr"])
-        
 
-        
+        location = {}
+        location["city"] = df.wohnort
+        location["zip"] = df.plz
+
+        person = Person(person_id=id,
+                        gender_concept_id=GENDER_D[last_record["geschlecht"]],
+                        year_of_birth=last_record["geburtsjahr"],
+                        race_concept_id="8552",  # Unknown
+                        ethnicity_concept_id="38003564",  # Non-Hispanic
+                        location_id=location,
+                        )
