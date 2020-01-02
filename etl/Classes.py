537b06666b205438fdb59b9697146107827c61cc
Markus Fritzsche <fritzsma111@gmail.com>
2020-01-02 18:50:10 +0100

    Finish measurment query generations

--- etl/Classes.py
+++ etl/Classes.py
@@ -2,5 +2,28 @@ class Person:
     def __init__(self, **meta_d):
         self.meta_d = meta_d
 
-    def add_measurements(self, **measurements):
-        self.measurements = measurements
+    def add_measurements(self, measurements):
+        self.measurements_d = measurements
+
+    def insert_into_db(self):
+        keys = ""
+        values = ""
+        for key, value in self.meta_d.items():
+            if key == "location": continue
+            keys += key + ","
+            values += value + ","
+        queries = ()
+        queries += f"INSERT INTO p21_cdm.person ({keys[:-1]}) VALUES({values[:-1]})",
+        #measurements
+        print (len(self.measurements_d))
+        for m in self.measurements_d:
+            keys = ""
+            values = ""
+            for key, value in m.items():
+                keys += key + ","
+                values += value + "," 
+            queries += f"INSERT INTO p21_cdm.measurements({keys[:-1]}) VALUES({values[:-1]})",
+        return queries
+
+
+
--- etl/main.py
+++ etl/main.py
@@ -25,24 +25,25 @@ if __name__ == "__main__":
         df = df.sort_values(by=["aufnahmedatum"])
         last_record = df.iloc[-1]
         location = {"city": df.wohnort, "zip": df.plz}
-        person = Person(person_id=id,
+        person = Person(person_id=str(id),
                         gender_concept_id=GENDER_D[last_record["geschlecht"]],
-                        year_of_birth=last_record["geburtsjahr"],
-                        month_of_birth=last_record["geburtsmonat"],
+                        year_of_birth=str(last_record["geburtsjahr"]),
+                        month_of_birth=str(last_record["geburtsmonat"]),
                         race_concept_id="8552",  # Unknown
                         ethnicity_concept_id="38003564",  # Non-Hispanic
-                        location_id=location,
+                        location=location,
                         )
         measurements = []
         kennzeichen_list = [k for k in df["kh_internes_kennzeichen"]]
         df = labor_csv[labor_csv.kh_internes_kennzeichen.isin(kennzeichen_list)]
         measurements = []
         for i, row in df.iterrows(): 
-            measurements.append({"measurement_concept_id" : row["LOINC"], 
-                                   "measurement_date" : row["timestamp"][:10], 
-                                   "measurement_datetime" : row["timestamp"], 
-                                   "measurement_type_concept_id" : row["LOINC"],
-                                   "value_as_number" : row["value"],
-                                   "range_low" : row["low"],
-                                   "range_high" : row["high"]})
-        person.add_measurements() 
+            measurements.append({"measurement_concept_id" : str(row["LOINC"]), 
+                                   "measurement_date" : str(row["timestamp"][:10]), 
+                                   "measurement_datetime" : str(row["timestamp"]), 
+                                   "measurement_type_concept_id" : str(row["LOINC"]),
+                                   "value_as_number" : str(row["value"]),
+                                   "range_low" : str(row["low"]),
+                                   "range_high" : str(row["high"])})
+        person.add_measurements(measurements)
+        print(person.insert_into_db())
