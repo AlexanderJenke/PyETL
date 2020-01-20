0ed01affe94cca5daeddec38b5d9fc8f51dfdf9b
Alexander Jenke <alexander.jenke@tu-dresden.de>
2020-01-20 15:28:45 +0100

    change training to Adam & SVM

--- ml/train.py
+++ ml/train.py
@@ -1,5 +1,5 @@
 import torch
-from torch.optim import SGD
+from torch.optim import SGD, Adam
 from torch.optim.lr_scheduler import OneCycleLR
 from torch.nn import MSELoss
 from torch.utils.data import DataLoader
@@ -10,11 +10,11 @@ import numpy as np
 from dataset import OMOP_Base
 from model import *
 
-N_EPOCHS = 100
+N_EPOCHS = 1000
 MAX_LR = 1e-3
-BATCH_SIZE = 1000
+BATCH_SIZE = 125
 
-epsilon = 1e-7
+epsilon = 1e-16
 
 
 def f1loss(input: torch.tensor, target: torch.tensor):
@@ -27,10 +27,17 @@ def f1_score(input: torch.tensor, target: torch.tensor):
     tp = torch.sum(pred * target)
     fp = torch.sum((1 - target) * pred)
     fn = torch.sum(target * (1 - pred))
+    tn = torch.sum((1 - pred) * (1 - target))
+
+    soft_f1_class1 = 2 * tp / (2 * tp + fn + fp + epsilon)
+    soft_f1_class0 = 2 * tn / (2 * tn + fn + fp + epsilon)
+    double_f1 = 0.5 * (soft_f1_class1 + soft_f1_class0)
+
     p = tp / (tp + fp + epsilon)
     r = tp / (tp + fn + epsilon)
     f1 = 2 * p * r / (p + r + epsilon)
-    return f1, p, r
+    assert (not torch.isinf(f1) or not torch.isnan(f1))
+    return f1, p, r, double_f1
 
 
 class wMSELoss:
@@ -53,19 +60,18 @@ if __name__ == '__main__':
 
     i = len(trainset[0][0])
 
-    model = N_FC(input_size=i,
-                 layers=[[200],
-                         [100, 1],
-                         # [i // 2, i // 4],
-                         # [i // 8, i // 16],
-                         # [i // 32, 1],
-                         ]
-                 ).to(device)
-    # model = Net_small(len(trainset[0][0])).to(device)
-    # model = Net(len(trainset[0][0])).to(device)
-    # model = SVM(len(trainset[0][0])).to(device)
+    # model = N_FC(input_size=i,
+    #              layers=[[i, 200],
+    #                      [1],
+    #                      # [i // 2, i // 4],
+    #                      # [i // 8, i // 16],
+    #                      # [i // 32, 1],
+    #                      ]
+    #              ).to(device)
 
-    optim = SGD(model.parameters(), lr=MAX_LR, momentum=0.9)
+    model = SVM(len(trainset[0][0])).to(device)
+
+    optim = Adam(model.parameters(), lr=MAX_LR)
     lr_sched = OneCycleLR(optim, max_lr=MAX_LR, epochs=N_EPOCHS, steps_per_epoch=len(trainloader))
     loss_f1 = f1loss
     loss_f1_score = f1_score
@@ -89,28 +95,30 @@ if __name__ == '__main__':
             preds_cat = torch.cat([preds_cat, output.detach().cpu()])
             labels_cat = torch.cat([labels_cat, labels_cpu])
 
-            # loss = loss_f1(output, labels) + loss_wMSE(output, labels) / 1e-2  # 5.6e-3
-            f1, p, r = loss_f1_score(output, labels)
+            f1, p, r, df1 = loss_f1_score(output, labels)
             wmse = loss_wMSE(output, labels)
+            mse = loss_MSE(output, labels)
 
-            loss = (torch.tensor(1) - p) + (torch.tensor(1) - r) / 100 + wmse
-
-            # loss = (torch.tensor(1) - p) / 100 + wmse
+            loss = (torch.tensor(1) - f1) + \
+                   wmse + \
+                   (torch.tensor(1) - r) / 100
 
             mean_loss.append(loss.item())
             loss.backward()
             optim.step()
             lr_sched.step()
 
-        f1, p, r = f1_score(preds_cat, labels_cat)
+        f1, p, r, df1 = f1_score(preds_cat, labels_cat)
 
         log.add_scalar("Precission", p, global_step=epoch)
         log.add_scalar("Recall", r, global_step=epoch)
         log.add_scalar("F1-Score", f1, global_step=epoch)
-        print(f"F1-Score: {f1:.5f}, {p:.5f}, {r:.5f}, {preds_cat.sum().item():.0f}")
+        log.add_scalar("Double F1-Score", df1, global_step=epoch)
+
+        print(f"F1-Score: {f1:.5f}, {p:.5f}, {r:.5f}, {preds_cat.sum().item():.0f} {df1:.5f}")
 
         log.add_scalar("Loss", np.mean(mean_loss), global_step=epoch)
         log.add_scalar("LR", lr_sched.get_lr(), global_step=epoch)
 
-    model.save("fc_i_i2_i4__i8_i16__i32_1.pt")
+    model.save("svm.pt")
     log.close()
