eb90c7ac8eda20854ef99e19b2155d2a4f845aa6
Alexander Jenke <alexander.jenke@tu-dresden.de>
2020-02-04 14:27:50 +0100

    add documentation to classes.py

--- etl/classes.py
+++ etl/classes.py
@@ -15,11 +15,13 @@ class Person:
                   visit_end_date,
                   visit_type_concept_id,
                   **kwargs):
+        """ Adds a visit entry to the patients data struct 
+        """
 
-        # add all optional Values
+        # add all optional values
         visit_d = {key: value for key, value in kwargs.items()}
 
-        # Required Values (Except person_id)
+        # Required values (Except person_id)
         visit_d["visit_occurrence_id"] = visit_occurrence_id
         visit_d["visit_concept_id"] = visit_concept_id
         visit_d["visit_start_date"] = visit_start_date
@@ -33,11 +35,13 @@ class Person:
                         measurement_date,
                         measurement_type_concept_id,
                         **kwargs):
+        """ Adds a measurement entry to the patients data struct 
+        """
 
-        # add all optional Values
+        # add all optional values
         measurement_d = {key: value for key, value in kwargs.items()}
 
-        # Required Values (Except measurement_id & person_id)
+        # Required values (Except measurement_id & person_id)
         measurement_d["measurement_concept_id"] = measurement_concept_id
         measurement_d["measurement_date"] = measurement_date
         measurement_d["measurement_type_concept_id"] = measurement_type_concept_id
@@ -50,17 +54,21 @@ class Person:
                         observation_date,
                         observation_type_concept_id,
                         **kwargs):
+        """ Adds a observation entry to the patients data struct
+        Does not insert the entry in the excact same data already exists. This mostly affects 'lokalisation' and
+        'diagnosensicherheit' entrys beeing doubled due to the existence on a 'sekundär_kode'.
+        """
 
-        # add all optional Values
+        # add all optional values
         observation_d = {key: value for key, value in kwargs.items()}
 
-        # Required Values (Except observation_id & person_id)
+        # Required values (Except observation_id & person_id)
         observation_d["observation_concept_id"] = observation_concept_id
         observation_d["observation_date"] = observation_date
         observation_d["observation_type_concept_id"] = observation_type_concept_id
 
         # do not insert recurrent information, reuse instead
-        # especially location definitions are affected by this
+        # especially 'lokalisation' & 'diagnosensicherheit' definitions are affected by this
         if observation_d in self.observations:
             return self.observations[self.observations.index(observation_d)]
 
@@ -73,11 +81,13 @@ class Person:
                       condition_start_datetime,
                       condition_type_concept_id,
                       **kwargs):
+        """ Adds a condition entry to the patients data struct
+        """
 
-        # add all optional Values
+        # add all optional values
         condition_d = {key: value for key, value in kwargs.items()}
 
-        # Required Values (Except condition_id & person_id)
+        # Required values (Except condition_id & person_id)
         condition_d["condition_concept_id"] = condition_concept_id
         condition_d["condition_start_date"] = condition_start_date
         condition_d["condition_start_datetime"] = condition_start_datetime
@@ -92,10 +102,13 @@ class Person:
                       procedure_datetime,
                       procedure_type_concept_id,
                       **kwargs):
-        # add all optional Values
+        """ Adds a procedure entry to the patients data struct
+        """
+
+        # add all optional values
         procedure_d = {key: value for key, value in kwargs.items()}
 
-        # Required Values (Except procedure_id & person_id)
+        # Required values (Except procedure_id & person_id)
         procedure_d["procedure_concept_id"] = procedure_concept_id
         procedure_d["procedure_date"] = procedure_date
         procedure_d["procedure_datetime"] = procedure_datetime
@@ -105,6 +118,9 @@ class Person:
         return procedure_d
 
     def add_fact_relationship(self, table_from: str, entry_from: dict, table_to: str, entry_to: dict):
+        """ Adds a fact relation entry to the patients data struct
+        """
+
         table_lut = {'p': "10",  # procedure
                      'c': "19",  # condition
                      'm': "21",  # measurement
@@ -112,6 +128,9 @@ class Person:
         self.fact_relations.append((table_lut[table_from], entry_from, table_lut[table_to], entry_to))
 
     def insert_into_db(self, database):
+        """ Inserts the collected patient data into the database
+        """
+
         # insert person
         keys = ""
         values = ""
@@ -174,7 +193,7 @@ class Person:
                 entry["sql_id"] = database.select(f"""INSERT INTO p21_cdm.{tablename}({keys[:-1]})
                                                       VALUES({values[:-1]}) RETURNING {tablename}_id""")[0][0]
 
-        # insert fact_relationships
+        # insert fact_relationships in both directions
         for table1, entry1, table2, entry2 in self.fact_relations:
             # 44818890 = Finding associated with (SNOMED)
             database.select(f"""INSERT INTO p21_cdm.fact_relationship(domain_concept_id_1, fact_id_1, 
@@ -187,4 +206,5 @@ class Person:
                                                                       relationship_concept_id)
                                 VALUES('{table2}','{entry2['sql_id']}','{table1}','{entry1['sql_id']}','44818792')""")
 
+        # make transactions persistent
         database.commit()
